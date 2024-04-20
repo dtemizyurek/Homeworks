e32b85e9e5df524074c705d0d80adf3d518af1cf
Doğukan Temizyürek <dogukantemizyurek@gmail.com>
2024-04-20 17:39:18 +0300

    Functions were written according to the documentation and various redirects were added

---
+++ BusBooking/BusBooking/Models/Routes.swift
@@ -0,0 +1,16 @@
+//
+//  Routes.swift
+//  BusBooking
+//
+//  Created by Doğukan Temizyürek on 20.04.2024.
+//
+
+import Foundation
+
+struct Routes {
+    let companyName: String
+    let companyImage: String
+    let price: String
+    let departurePoint: String
+    let destinationPoint: String
+}
--- BusBooking/BusBooking/Models/Ticket.swift
+++ BusBooking/BusBooking/Models/Ticket.swift
@@ -13,16 +13,40 @@ struct Ticket: GiveInformationProtocol {
     let ticket: [Ticket]
     let time: [Time]
     let date: [Date]
-    let seat: Int
-    let reservedSeat: Int
+    var seat: [Int]?
+    let reservedSeat = 0
     
-    private func compareTickets() {
-    
+    // İki biletin koltuklarını karşılaştıran özel bir fonksiyon
+    private func compareTickets(otherTicket: Ticket) -> Bool {
+        guard let thisTicketSeats = seat else {
+            print("The seats for this ticket are still empty. No comparison can be made.")
+        }
         
+        guard let otherTicketSeats = otherTicket.seat else {
+            print("The seats for the other ticket have not yet been assigned. No comparison can be made.")
+        }
+        
+        for seatNumber in thisTicketSeats {
+            if otherTicketSeats.contains(seatNumber) {
+                return true
+            }
+        }
+        return false
     }
-    
-    private func reserveSeat() {
-        
+    // Koltuk rezervasyonu yapanl fonksiyon
+    private func reserveSeat(seat: Int) -> [Int] {
+        if reservedSeat > 0 {
+           return self.seat!
+        }else {
+            let tempArray = repeatElement(0, count: seat)
+            let tempIntArray = tempArray.map {(Int($0))}
+            return tempIntArray
+        }
+    }
+
+    // Koltuk ekleme işlemini gerçekleştiren fonksiyon
+    mutating func addSeat(willReserveSeat: Int) {
+        seat!.append(willReserveSeat)
     }
     
     
